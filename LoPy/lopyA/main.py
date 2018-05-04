34adeb6e28ccb9e5318d1c304930e70df6631899
Veena Balakumar <v241091@gmail.dk>
2018-05-04 08:50:50 +0200

    LoRaWAN

--- LoPy/lopyA/main.py
+++ LoPy/lopyA/main.py
@@ -9,3 +9,25 @@ for cycles in range(10):  # stop after 10 cycles
     pycom.rgbled(0x7f0000)  # red
     time.sleep(4)
     print("light")
+
+""" LoPy LoRaWAN Nano Gateway example usage """
+
+import config
+from nanogateway import NanoGateway
+
+if __name__ == '__main__':
+    nanogw = NanoGateway(
+        id=config.GATEWAY_ID,
+        frequency=config.LORA_FREQUENCY,
+        datarate=config.LORA_GW_DR,
+        ssid=config.WIFI_SSID,
+        password=config.WIFI_PASS,
+        server=config.SERVER,
+        port=config.PORT,
+        ntp_server=config.NTP,
+        ntp_period=config.NTP_PERIOD_S
+        )
+
+    nanogw.start()
+    nanogw._log('You may now press ENTER to enter the REPL')
+    input()
